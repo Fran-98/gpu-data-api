fa39a786b94615132633f1b495c4b58777f658c6
Francisco <Franciscopvargas98@gmail.com>
2023-09-27 00:21:59 -0300

    Added a little example of an endpoint

--- gpu-data-api/main.py
+++ gpu-data-api/main.py
@@ -1,13 +1,18 @@
 import db
 from models import gpuBenchmarks
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 def run():
-    
+    app.run()
     pass
+
+@app.route('/gpu_info', methods=['GET'])
+def gpu_info():
+    args = request.args
+    return args
 
 
 if __name__ == '__main__':
