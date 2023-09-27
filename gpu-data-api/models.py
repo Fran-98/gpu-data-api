dcc4e807b3d8f9a6b488fae4a1416f05452509df
cazalGz <giotapping@gmail.com>
2023-09-27 19:17:28 -0300

    Primeras routes, models actualizado

--- gpu-data-api/models.py
+++ gpu-data-api/models.py
@@ -20,3 +20,46 @@ class gpuBenchmarks(db.Base):
 
     def __str__(self):
         return self.name
+    
+
+
+
+class gpu_spec(db.Base):
+    __tablename__ = 'gpu_spec'
+    manufacturer = Column(String(30))
+    gpuName = Column(String(50), primary_key=True)
+    releaseYear = Column(Integer)
+    memSize=Column(Integer)
+    memBusWidth=Column(Integer)
+    gpuClock=Column(Integer)
+    unifiedShader=Column(Integer)
+    tmu=Column(Integer)
+    rop=Column(Integer)
+    pixelShader=Column(String(10))
+    ipg=Column(String(10))
+    bus=Column(String(30))
+    memType=Column(String(20))
+    gpuChip=Column(String(20))
+
+
+    def __init__(self, manufacturer, gpuName, releaseYear, memSize, memBusWidth, gpuClock, unifiedShader, tmu, rop, pixelShader, ipg, bus, memType, gpuChip):
+        self.manufacturer = manufacturer
+        self.gpuName = gpuName
+        self.releaseYear = releaseYear
+        self.memSize = memSize
+        self.memBusWidth = memBusWidth
+        self.gpuClock = gpuClock
+        self.unifiedShader = unifiedShader
+        self.tmu = tmu
+        self.rop = rop
+        self.pixelShader = pixelShader
+        self.ipg = ipg
+        self.bus = bus
+        self.memType = memType
+        self.gpuChip = gpuChip
+       
+    def __repr__(self):
+        return f'{self.gpuName}, manufacturer: {self.manufacturer}, releaseYear: {self.releaseYear}, memSize: {self.memSize},memBusWidth: {self.memBusWidth}, gpuClock: {self.gpuClock},unifiedShader: {self.unifiedShader}, tmu: {self.tmu}, rop: {self.rop}, pixelShader: {self.pixelShader}, ipg: {self.ipg}, bus: {self.bus}, memType: {self.memType}, gpuChip: {self.gpuChip}'
+
+    def __str__(self):
+        return self.gpuName
