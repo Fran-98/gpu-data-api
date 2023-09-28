c6646849499e40eb5156ea27554b6b14b6bc8595
cazalGz <giotapping@gmail.com>
2023-09-28 10:06:49 -0300

    correccion de archivo db.py

--- gpu-data-api/db.py
+++ gpu-data-api/db.py
@@ -3,7 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-engine = create_engine(f"mysql+mysqlconnector://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@localhost/gpu_db")
+
+user="{os.environ['DB_USER']}"
+password="{os.environ['DB_PASSWORD']}"
+
+
+engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@localhost/gpu_db")
 Session = sessionmaker(bind=engine)
 session = Session()
 
